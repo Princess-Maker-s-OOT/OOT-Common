105e081f05be47bdd8839ab8baa69ee37e60a8e5
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-11-16 03:39:15 +0900

    [refactor] dto: 캐싱적용 dto 수정

--- src/com/ootcommon/dashboard/response/DashboardUserSummaryResponse.java
+++ src/com/ootcommon/dashboard/response/DashboardUserSummaryResponse.java
@@ -1,15 +1,24 @@
 package com.ootcommon.dashboard.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ootcommon.category.response.CategoryStat;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class DashboardUserSummaryResponse {
 
     private final int totalClothes; // 등록된 옷 전체 수
     private final List<CategoryStat> categoryStat; // 카테고리별 옷 개수 limit 10
+
+    @JsonCreator
+    public DashboardUserSummaryResponse(
+            @JsonProperty("totalClothes") int totalClothes,
+            @JsonProperty("categoryStat") List<CategoryStat> categoryStat
+    ) {
+        this.totalClothes = totalClothes;
+        this.categoryStat = categoryStat;
+    }
 }
